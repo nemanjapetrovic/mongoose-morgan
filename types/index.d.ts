efd1a0d75218bc3fd6850d640008bd3fcfb3ccdf
nemanjapetrovic <nempet@protonmail.com>
2020-05-31 14:20:07 +0200

    Added typescript types

---
+++ types/index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * MongooseMorgan object
+ * @param  {object} mongoData - represents mongo database data, requires { connectionString : '{MONGO_URL}' } parameter.
+ * @param  {object} options - represents morgan options, check their github, default value is empty object {}.
+ * @param  {string} format - represents morgan formatting, check their github, default value is 'combined'.
+ */
+export declare function MongooseMorgan(mongoData: object, options: object, format: string): object
